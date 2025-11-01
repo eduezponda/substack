b5ade70b8e978ec3c730f1ffaf8d8d81a04c79f9
Eduardo Ezponda <eduezponda@gmail.com>
2025-11-01 18:06:58 +0100

    Modify business objective section

--- frontend/src/components/Home/SectionBusinessObjective.tsx
+++ frontend/src/components/Home/SectionBusinessObjective.tsx
@@ -2,15 +2,16 @@ export default function SectionWhatWereNot() {
   return (
     <section className="w-full bg-white px-4 py-8">
       <h2 className="text-[#004AAD] text-2xl font-bold mb-4 tracking-tight">
-        Discipline, not brilliance
+        From the best, to you.
       </h2>
-      <p className="text-[#004AAD] text-xl leading-relaxed font-normal max-w-2xl">
-        We’re building something different. A place for people who care about learning, thinking, and investing intelligently.
-        <div className="mt-2">
-          You don't need to be smarter than the rest — just more disciplined
-        </div>
-      </p>
-      
+      <div className="text-[#004AAD] text-xl leading-relaxed font-normal max-w-2xl space-y-4">
+        <p>
+          We’ve learned from Spain’s top fund managers.
+        </p>
+        <p>
+          Our goal is simple: follow quality, think like the greats, and save you the years it took us to learn it.
+        </p>
+      </div>
     </section>
   );
 }
