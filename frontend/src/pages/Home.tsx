47f8e05cb581c8b21baf937ed21b9886c3e7c64d
Eduardo Ezponda <eduezponda@gmail.com>
2025-11-09 18:29:36 +0100

    Add new components in home

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,16 +1,20 @@
 import Header from "@/components/Home/Header";
 import Hero from "@/components/Home/Hero";
-import SectionBusinessObjective from "@/components/Home/SectionBusinessObjective";
-import SectionWhatWereNot from "@/components/Home/SectionWhatWereNot";
+import SectionBusinessObjective from "@/components/Home/BusinessObjectiveSection";
+import SectionWhatWereNot from "@/components/Home/WhatWereNotSection";
+import BlogCarouselSection from "@/components/Home/BlogCarouselSection";
 
 export default function Home() {
   return (
     <div className="border-0 border-none">
       <Header />
       <Hero />
-      <SectionBusinessObjective />
-      <SectionWhatWereNot />
-
+      <main>
+        <SectionBusinessObjective />
+        <SectionWhatWereNot />
+        <BlogCarouselSection />
+        <div className="w-full h-16 bg-red-500">Mazapan</div>
+      </main>
     </div>
   );
 }
